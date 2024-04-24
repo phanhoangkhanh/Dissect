98007be6d768c6efea0f94c721672d679e94f546
phan hoang khanh <95729182+phanhoangkhanh@users.noreply.github.com>
2024-04-24 22:10:41 +0700

    Update web.php

--- package/module/routes/web.php
+++ package/module/routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('api/khanh', function () {
-    dd('CHUNG TA DA THANH CONG - main chinh sửa -0 sẽ xung dot voi develop');
+    dd('CHUNG TA DA THANH CONG - main chinh sửa -0 sẽ xung dot voi develop- hiep 2');
 });
